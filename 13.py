a28e73c6f968d6ef5e269195ac18e5e1b35cc14c
RiimoH <remo.herzog@hotmail.com>
2023-12-13 15:01:53 +0100

    day13 solved

--- 13.py
+++ 13.py
@@ -60,47 +60,44 @@ def part_one(inp):
 def part_two(inp):
     def parse(inp):
         inp = inp.split("\n\n")
-        output = []
-        for mirror in inp:
-            mirror = [
-                int(x, 2)
-                for x in mirror.replace("#", "1").replace(".", "0").split("\n")
-            ]
-
-            output.append(mirror)
-        return output
+        inp = [x.split("\n") for x in inp]
+        return inp
 
     def find_horizontal_reflection(inp):
         for i in range(len(inp) - 1):
-            pairs = list()
-            for d in range(min(i + 1, len(inp) - i - 1)):
-                try:
-                    x, y = inp[i - d], inp[i + 1 + d]
-                except IndexError:
-                    ic(inp)
-                    ic(i, d, len(inp))
-                    pairs.append(2)
+            o, u = i, i + 1
+            smudge = False
+            while True:
+                comp = compare(inp[o], inp[u])
+                if comp > 1:
                     break
-                comp = x ^ y
-                count = 0
-                while comp:
-                    comp = comp & (comp - 1)
-                    count += 1
-                pairs.append(count)
-                if count > 1:
-                    break
+                elif comp == 1:
+                    if smudge:
+                        break
+                    else:
+                        smudge = True
+                        
+                o -= 1
+                u += 1
 
-            if sum(pairs) == 1:
-                return i + 1
-
-        return -1
+                if o < 0 or u >= len(inp):
+                    if smudge:
+                        return i + 1
+                    else:
+                        break
+        else:
+            return -1
 
     def find_vertical_reflection(inp):
-        inp = list(map(bin, inp))
-        inp = [int("".join(x), 2) for x in list(zip(*inp))[2:]]
+        inp = ["".join(x) for x in zip(*inp)]
         return find_horizontal_reflection(inp)
 
     inp = parse(inp)
+
+    def compare(x, y):
+        comp = [1 if x[idx] != y[idx] else 0 for idx in range(len(x))]
+        return sum(comp)
+
 
     ts = 0
     for part in inp:
@@ -116,6 +113,58 @@ def part_two(inp):
     return ts
 
 
+# def part_two(inp):
+#     def parse(inp):
+#         inp = inp.split("\n\n")
+#         output = []
+#         for mirror in inp:
+#             mirror = [
+#                 int(x, 2)
+#                 for x in mirror.replace("#", "1").replace(".", "0").split("\n")
+#             ]
+
+#             output.append(mirror)
+#         return output
+
+#     def find_horizontal_reflection(inp):
+#         for i in range(len(inp) - 1):
+#             pairs = []
+#             for d in range(min(i + 1, len(inp) - i - 1)):
+            
+#                 x, y = inp[i - d], inp[i + 1 + d]
+#                 comp = x ^ y
+#                 count = bin(comp).count('1')
+#                 ic(x, y, comp, count)
+#                 pairs.append(count)
+#                 if count > 1:
+#                     break
+
+#             if sum(pairs) == 1:
+#                 return i + 1
+
+#         return -1
+
+#     def find_vertical_reflection(inp):
+#         inp = [format(x, '09b') for x in inp]
+#         inp = [int("".join(x), 2) for x in list(zip(*inp))[2:]]
+#         return find_horizontal_reflection(inp)
+
+#     inp = parse(inp)
+
+#     ts = 0
+#     for part in inp:
+#         x = find_horizontal_reflection(part)
+#         if x >= 0:
+#             ts += x * 100
+#             continue
+#         x = find_vertical_reflection(part)
+#         if x >= 0:
+#             ts += x
+#             continue
+
+#     return ts
+
+
 with open("13.inp") as fp:
     inp = fp.read()
 
@@ -125,4 +174,4 @@ with open("13.inp") as fp:
 
 print("Test Two:", part_two(test))
 
-print("Part Two:", part_two(inp), "> 36115")
+print("Part Two:", part_two(inp), "> 36150")
