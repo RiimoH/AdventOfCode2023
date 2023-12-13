e686fd51d8fe86dadcc288efcdca636a473f961c
RiimoH <remo.herzog@hotmail.com>
2023-12-13 21:31:11 +0100

    asdfasdf

--- 12.py
+++ 12.py
@@ -1,9 +1,9 @@
 from icecream import ic
+from functools import cache
+from typing import List, Tuple
+from math import prod
 import re
-from functools import lru_cache
-from typing import List
 
-re_replace = r"\.+"
 
 test = """???.### 1,1,3
 .??..??...?##. 1,1,3
@@ -23,66 +23,92 @@ def parse(inp):
 
     return rows
 
-@lru_cache
-def brute(row: str, nums: List[int]):
-    if len(nums) == 0:
-        return 1
+
+@cache
+def brute(record: str, groups: Tuple[int]):
+    if not groups:
+        if "#" not in record:
+            return 1
+        else:
+            return 0
+
+    if not record:
+        return 0
+    
+    record = record.lstrip('.')
+
+    if len(groups) == 1:
+        return sum(record.startswith(groups[0], i) for i, _ in enumerate(record))
+
+    if record[0] == '#':
+        next_group = groups[0]
+
+        if "." in record[:next_group] or "#" in record[next_group]:
+            return 0
+
+        return brute(record[next_group+1:], groups[1:])
+    
+    else: # record is '?'
+        # if ? is .
+        
+
 
     s = 0
 
-    s += brute('.'+row[1:], nums)
-    num = nums[0]
-    if row.startswith('#'*num) and row[num] != '#':
-        s += brute(row[num+2:].lstrip('.'), nums[1:])
+    s += brute(record[1:], groups)
+    num = groups[0]
+
+    take, go_on = record[:num], record[num:]
+    if "." not in take and (go_on.startswith(".") or go_on.startswith("?")):
+        s += brute(record[num + 1 :].lstrip("."), groups[1:])
 
     return s
 
-# @lru_cache
-# def find_combinations(rows, nums):
-#     if len(nums) == 1 or len(rows) == 1:
 
-#         if nums[0] > max(map(len, rows)):
-#             return 0
-#         else:
-#             return sum(len(row) - nums[0] + 1 for row in rows)
+def find_combinations(rows: List[str], nums: List[int]):
+    if len(nums) == 0:
+        return 1
 
-#     if sum(nums) + len(nums) - 1 > len(rows):  # there are not enough spaces to process
-#         return 0
+    max_int: int = max(nums)
+    if nums.count(max_int) == 1:
+        factors: List[int] = []
+        section_lengths = [len(x) for x in rows]
 
-#     factors = []
+        if section_lengths.count(max_int) == 1:
+            factors.append(
+                find_combinations(
+                    rows[: section_lengths.index(max_int)], nums[: nums.index(max_int)]
+                )
+            )
+            factors.append(
+                find_combinations(
+                    rows[section_lengths.index(max_int) + 1 :],
+                    nums[nums.index(max_int) + 1 :],
+                )
+            )
+        return prod(factors)
 
-#     max_int = max(nums)
-#     if nums.count(max_int) == 1:
-#         row_lens = [len(x) for x in rows]
-#         if row_lens.count(max_int) == 1:
-#             factors.append(
-#                 find_combinations(
-#                     rows[: row_lens.index(max_int)], nums[: nums.index(max_int)]
-#                 )
-#             )
-#             factors.append(
-#                 scd=find_combinations(
-#                     rows[row_lens.index(max_int) + 1], nums[nums.index(max_int) + 1 :]
-#                 )
-#             )
-
-#     for idx in range(len(rows)):
-#         find_combinations(rows[idx + len(nums[0]) :], nums[1:])
+    return brute(".".join(rows), tuple(nums))
 
 
 def part_one(inp):
-    rows = ic(parse(inp))
-
+    rows = parse(inp)
+    s = 0
     for row, nums in rows:
-        row = re.sub(re_replace, ".", row, re.MULTILINE)
+        row = re.sub(r"\.+", ".", row, re.MULTILINE)
         row = row.strip(".").rstrip(".")
         row = row.split(".")
-        for i, seq in enumerate(row):
-            if "?" not in seq:
-                row[i] = len(seq)
 
-        lint = max(nums)
-        lint_first_idx = nums.index(lint)
+        nrow = []
+        for i, seq in enumerate(row):
+            if "?" not in seq and nums.count(len(seq)) == 1:
+                nums.remove(len(seq))
+            else:
+                nrow.append(seq)
+
+        s += ic(find_combinations(nrow, tuple(nums)))
+
+    return s
 
 
 def part_two(inp):
