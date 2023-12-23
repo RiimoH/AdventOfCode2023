56f091b0d68ca73ed0de10f12a6d5971308d68c7
RiimoH <remo.herzog@hotmail.com>
2023-12-24 00:27:50 +0100

    day23 done

--- 23.py
+++ 23.py
@@ -1,6 +1,4 @@
 from icecream import ic
-import re
-
 
 test = """#.#####################
 #.......#########...###
@@ -39,17 +37,19 @@ def parse(inp):
 
 def part_one(inp):
     def generate_sections(section):
-        pc, pr = section[-1]
+        pr, pc = section[-1]
 
-        for dc, dr in dirs.values():
-            npos = pc + dc, pr + dr
+        for dr, dc in dirs.values():
+            npos = pr + dr, pc + dc
 
             if npos in section:
                 continue
 
             if npos in crossings:
-                sections.append((section + [npos], len(section)))
-                continue
+                s = sections.get(section[0], dict())
+                s[npos] = len(section)
+                sections[section[0]] = s
+                return
 
             nt = board.get(npos, "#")
 
@@ -69,6 +69,17 @@ def part_one(inp):
 
         return crossings
 
+    def find_combinations(route, steps=0):
+        if route[-1] == goal:
+            routes.append(steps)
+            return
+
+        for (target), step in sections[route[-1]].items():
+            if target in route:
+                continue
+
+            find_combinations(route + [target], steps + step)
+
     dirs = {
         ">": (0, 1),
         "v": (1, 0),
@@ -77,7 +88,7 @@ def part_one(inp):
     }
 
     board, start, goal = parse(inp)
-    sections = []
+    sections = {}
 
     crossings = generate_crossings(
         {
@@ -92,10 +103,86 @@ def part_one(inp):
         )
 
     ic(sections)
+    ic(goal)
+    routes = []
+    find_combinations([start])
+
+    return max(routes)
 
 
 def part_two(inp):
-    ...
+    def generate_sections(section):
+        pr, pc = section[-1]
+
+        for dr, dc in dirs.values():
+            npos = pr + dr, pc + dc
+
+            if npos in section:
+                continue
+
+            if npos in crossings:
+                s = sections.get(section[0], dict())
+                s[npos] = len(section)
+                sections[section[0]] = s
+                return
+
+            nt = board.get(npos, "#")
+
+            if nt == "." or nt in dirs.keys() :
+                generate_sections(section + [npos])
+
+    def generate_crossings(crossings):
+        for (r, c), tile in board.items():
+            if tile == ".":
+                neighbors = []
+                for dr, dc in dirs.values():
+                    if (nt := board.get((r + dr, c + dc), "#")) != "#":
+                        neighbors.append(nt)
+
+                if len(neighbors) > 2:
+                    crossings.add((r, c))
+
+        return crossings
+
+    def find_combinations(route, steps=0):
+        if route[-1] == goal:
+            routes.append(steps)
+            return
+
+        for (target), step in sections[route[-1]].items():
+            if target in route:
+                continue
+
+            find_combinations(route + [target], steps + step)
+
+    dirs = {
+        ">": (0, 1),
+        "v": (1, 0),
+        "<": (0, -1),
+        "^": (-1, 0),
+    }
+
+    board, start, goal = parse(inp)
+    sections = {}
+
+    crossings = generate_crossings(
+        {
+            start,
+            goal,
+        }
+    )
+    ic(crossings)
+    for s in crossings:
+        generate_sections(
+            [s],
+        )
+
+    ic(sections)
+    ic(goal)
+    routes = []
+    find_combinations([start])
+
+    return max(routes)
 
 
 with open("23.inp") as fp:
@@ -103,8 +190,11 @@ with open("23.inp") as fp:
 
 print("Test One:", part_one(test))
 
-# print("Part One:", part_one(inp))
+ic.disable()
+print("Part One:", part_one(inp))
 
-# print("Test Two:", part_two(test))
+ic.enable()
+print("Test Two:", part_two(test))
 
-# print("Part Two:", part_two(inp))
+ic.disable()
+print("Part Two:", part_two(inp))
